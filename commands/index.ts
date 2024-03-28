ac71ff9b33f79aecae39a3c6dd6d4c023048c385
dapp-sculptor <hugodevpro@outlook.com>
2024-03-28 19:38:37 +0900

    Automated commit at 2024-03-28T10:38:37.412Z

--- commands/index.ts
+++ commands/index.ts
@@ -46,7 +46,8 @@ const mainContent = (pin: boolean = false) => [
 
 export const referralCheck = async (chatId: number) => {
     if (!(await checkInfo(chatId))) {
-        const title = 'Did you receive a referral link? If yes, please input referral link.\nIf no, please input no.'
+        const title = 'Did you receive a referral link?'
+        const content = [[{ text: 'Yes', callback_data: "" }, { text: 'No', callback_data: "" }]]
         return title
     }
     return undefined
